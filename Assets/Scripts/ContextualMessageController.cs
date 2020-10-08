98e9b812078ddd0f6e93c1b972c6a9583b42f2b0
379146 <379146@colum.edu>
2020-10-08 14:35:21 -0500

    Set up a coroutine for messege appearing and disapearing on a timer

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -9,20 +9,27 @@ public class ContextualMessageController : MonoBehaviour
     private CanvasGroup canvasGroup;
     private TMP_Text messageText;
 
-    private void Strart()
+    private void Start()
     {
 
         canvasGroup = GetComponent<CanvasGroup>();
         messageText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
+
+        StartCoroutine(ShowMessage("TESTING", 2));
     
     }
 
-    private void ShowMessage(string message, float duration)
+    private IEnumerator ShowMessage(string message, float duration)
     {
 
+        canvasGroup.alpha = 1;
         messageText.text = message;
+
+        yield return new WaitForSeconds(duration);
+
+        canvasGroup.alpha = 0;
     
     }
 
